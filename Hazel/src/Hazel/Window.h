15e6e7043fb52be56480d481feec12c20416912d
Fallshadow <www.sunshuchao@outlook.com>
2024-04-12 12:26:08 +0800

    add 获取对应窗口指针的函数

--- Hazel/src/Hazel/Window.h
+++ Hazel/src/Hazel/Window.h
@@ -32,6 +32,9 @@ namespace Hazel
 		virtual void SetEventCallback(const EventCallbackFn& callback) = 0;
 		virtual void SetVSync(bool enabled) = 0;
 		virtual bool IsVSync() const = 0;
+		
+		// 添加获取活动窗口的公开函数，以便于各种类型的窗口获取需求
+		virtual void* GetNativeWindow() const = 0;
 
 		// 创建窗口
 		static Window* Create(const WindowProps& props = WindowProps());
