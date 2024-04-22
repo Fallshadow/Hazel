ebad68eb927bc80ba47206ab76355ba7483e5382
Fallshadow <www.sunshuchao@outlook.com>
2024-04-22 17:47:04 +0800

    update layer attach and detach move
    将层级的实际应用和卸载放到layerstack中去做，且注意要先卸载再清除

--- Hazel/src/Hazel/LayerStack.cpp
+++ Hazel/src/Hazel/LayerStack.cpp
@@ -20,12 +20,14 @@ namespace Hazel
 	{
 		m_Layers.emplace(m_Layers.begin() + m_LayerInsertIndex, layer);
 		m_LayerInsertIndex++;
+		layer->OnAttach();
 	}
 
 	// 推入覆盖层，推到栈底，迭代器不动
 	void LayerStack::PushOverlay(Layer* overlay)
 	{
 		m_Layers.emplace_back(overlay);
+		overlay->OnAttach();
 	}
 
 	// 弹出普通层，迭代器向下减少
@@ -34,6 +36,7 @@ namespace Hazel
 		auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
 		if (it != m_Layers.end())
 		{
+			layer->OnDetach();
 			m_Layers.erase(it);
 			m_LayerInsertIndex--;
 		}
@@ -45,6 +48,7 @@ namespace Hazel
 		auto it = std::find(m_Layers.begin(), m_Layers.end(), overlay);
 		if (it != m_Layers.end())
 		{
+			overlay->OnDetach();
 			m_Layers.erase(it);
 		}
 	}
