05a7b88ae4692a05369bbffc060de82d15a298e8
Fallshadow <www.sunshuchao@outlook.com>
2024-05-07 15:24:27 +0800

    update 空构造函数改为默认构造函数

--- Hazel/src/Hazel/Events/ApplicationEvent.h
+++ Hazel/src/Hazel/Events/ApplicationEvent.h
@@ -7,7 +7,7 @@ namespace Hazel
 	class WindowCloseEvent : public Event
 	{
 	public:
-		WindowCloseEvent() {}
+		WindowCloseEvent() = default;
 
 		EVENT_CLASS_TYPE(WindowClose)
 		EVENT_CLASS_CATEGORY(EventCategoryApplication)
@@ -37,7 +37,7 @@ namespace Hazel
 	class AppTickEvent : public Event
 	{
 	public:
-		AppTickEvent() {}
+		AppTickEvent() = default;
 
 		EVENT_CLASS_TYPE(AppTick)
 		EVENT_CLASS_CATEGORY(EventCategoryApplication)
@@ -46,7 +46,7 @@ namespace Hazel
 	class AppUpdateEvent : public Event
 	{
 	public:
-		AppUpdateEvent() {}
+		AppUpdateEvent() = default;
 
 		EVENT_CLASS_TYPE(AppUpdate)
 		EVENT_CLASS_CATEGORY(EventCategoryApplication)
@@ -55,7 +55,7 @@ namespace Hazel
 	class AppRenderEvent : public Event
 	{
 	public:
-		AppRenderEvent() {}
+		AppRenderEvent() = default;
 
 		EVENT_CLASS_TYPE(AppRender)
 		EVENT_CLASS_CATEGORY(EventCategoryApplication)
