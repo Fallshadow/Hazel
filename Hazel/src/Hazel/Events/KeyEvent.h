7e24a5cdb0eca3d09d5b4a22690cc3a1c108f1c1
Fallshadow <www.sunshuchao@outlook.com>
2024-05-09 11:00:01 +0800

    update 按键宏定义替换为枚举

--- Hazel/src/Hazel/Events/KeyEvent.h
+++ Hazel/src/Hazel/Events/KeyEvent.h
@@ -1,25 +1,26 @@
 #pragma once
 
 #include "Hazel/Events/Event.h"
+#include "Hazel/Core/Input.h"
 
 namespace Hazel
 {
 	class KeyEvent : public Event
 	{
 	public:
-		inline int GetKeyCode() const { return m_KeyCode; }
+		inline KeyCode GetKeyCode() const { return m_KeyCode; }
 
 		EVENT_CLASS_CATEGORY(EventCategoryKeyboard | EventCategoryInput)
 	protected:
 		// 按键基类不应该被实例化，所以保护起来
-		KeyEvent(int keycode) : m_KeyCode(keycode) { }
-		int m_KeyCode;
+		KeyEvent(KeyCode keycode) : m_KeyCode(keycode) { }
+		KeyCode m_KeyCode;
 	};
 
 	class KeyPressedEvent : public KeyEvent
 	{
 	public:
-		KeyPressedEvent(int keycode, int repeatCount) : KeyEvent(keycode), m_RepeatCount(repeatCount) { }
+		KeyPressedEvent(KeyCode keycode, int repeatCount) : KeyEvent(keycode), m_RepeatCount(repeatCount) { }
 		inline int GetRepeatCount() const { return m_RepeatCount; }
 
 		std::string ToString() const override
@@ -36,7 +37,7 @@ namespace Hazel
 	class KeyReleasedEvent : public KeyEvent
 	{
 	public:
-		KeyReleasedEvent(int keycode) : KeyEvent(keycode) { }
+		KeyReleasedEvent(KeyCode keycode) : KeyEvent(keycode) { }
 		std::string ToString() const override
 		{
 			std::stringstream ss;
@@ -49,7 +50,7 @@ namespace Hazel
 	class KeyTypedEvent : public KeyEvent
 	{
 	public:
-		KeyTypedEvent(int keycode) : KeyEvent(keycode) {}
+		KeyTypedEvent(KeyCode keycode) : KeyEvent(keycode) {}
 		std::string ToString() const override
 		{
 			std::stringstream ss;
