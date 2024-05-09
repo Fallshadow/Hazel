4773259dc5963965738e1089573cfa819088579e
Fallshadow <www.sunshuchao@outlook.com>
2024-05-09 11:28:36 +0800

    update 引擎对外部隐藏Run

--- Hazel/src/Hazel/Core/Application.h
+++ Hazel/src/Hazel/Core/Application.h
@@ -10,6 +10,8 @@
 #include "Hazel/Core/Timestep.h"
 #include "Hazel/ImGui/ImGuiLayer.h"
 
+int main(int argc, char** argv);
+
 namespace Hazel
 {
 	class Application
@@ -18,7 +20,7 @@ namespace Hazel
 		Application();
 		virtual ~Application();
 
-		void Run();
+
 		void OnEvent(Event& e);
 
 		void PushLayer(Layer* layer);
@@ -28,6 +30,7 @@ namespace Hazel
 
 		inline static Application& Get() { return *s_Instance; }
 	private:
+		void Run();
 		bool OnWindowClose(WindowCloseEvent& e);
 		bool OnWindowResize(WindowResizeEvent& e);
 	private:
@@ -39,6 +42,7 @@ namespace Hazel
 		float m_LastFrameTime = 0.0f;
 	private:
 		static Application* s_Instance;
+		friend int ::main(int argc, char** argv);
 	};
 
 	// To be defined in CLIENT
