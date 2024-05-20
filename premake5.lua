8c1a7cbcf51466ea543fa474002a863fba4c2aba
Fallshadow <www.sunshuchao@outlook.com>
2024-05-20 12:06:52 +0800

    update rename nut and fit resize

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,6 @@
 workspace "Hazel"
 	architecture("x86_64")
-	startproject "Hazel-Editor"
+	startproject "Hazelnut"
 
 	configurations
 	{
@@ -157,8 +157,8 @@ project "Sandbox"
 		runtime "Release"
 		optimize "on"
 
-project "Hazel-Editor"
-	location "Hazel-Editor"
+project "Hazelnut"
+	location "Hazelnut"
 	kind "ConsoleApp"
 	language "C++"
 	cppdialect "C++17"
