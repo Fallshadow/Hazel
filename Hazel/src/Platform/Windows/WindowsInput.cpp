0d065fac7f67dea03dd7ceedb3339052eaa1853d
Fallshadow <www.sunshuchao@outlook.com>
2024-05-23 09:49:04 +0800

    update key修改 & GetMousePosition弃用pair返回glm::vec2

--- Hazel/src/Platform/Windows/WindowsInput.cpp
+++ Hazel/src/Platform/Windows/WindowsInput.cpp
@@ -20,7 +20,7 @@ namespace Hazel
 		return state == GLFW_PRESS;
 	}
 
-	std::pair<float, float> Input::GetMousePosition()
+	glm::vec2 Input::GetMousePosition()
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		double x, y;
@@ -30,13 +30,11 @@ namespace Hazel
 
 	float Input::GetMouseX()
 	{
-		auto [x, y] = GetMousePosition();
-		return x;
+		return GetMousePosition().x;
 	}
 
 	float Input::GetMouseY()
 	{
-		auto [x, y] = GetMousePosition();
-		return y;
+		return GetMousePosition().y;
 	}
 }
