682c2d6e5c3dfdd181c1c1973452e141f0354103
Fallshadow <www.sunshuchao@outlook.com>
2024-05-27 16:04:08 +0800

    update initializer_list 不需要再引用 & 增加 const

--- Hazel/src/Hazel/Renderer/Buffer.h
+++ Hazel/src/Hazel/Renderer/Buffer.h
@@ -68,7 +68,7 @@ namespace Hazel
 	{
 	public:
 		BufferLayout() { }
-		BufferLayout(const std::initializer_list<BufferElement>& elements) : m_Elements(elements)
+		BufferLayout(std::initializer_list<BufferElement> elements) : m_Elements(elements)
 		{
 			CalculateOffsetsAndStride();
 		}
