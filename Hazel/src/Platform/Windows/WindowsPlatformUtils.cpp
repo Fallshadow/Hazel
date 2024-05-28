5a51b148f9e65bf9697b4fe79487c9c0bf68d0cd
Fallshadow <www.sunshuchao@outlook.com>
2024-05-28 18:33:14 +0800

    update 改用optional & 遗漏flags

--- Hazel/src/Platform/Windows/WindowsPlatformUtils.cpp
+++ Hazel/src/Platform/Windows/WindowsPlatformUtils.cpp
@@ -11,7 +11,7 @@
 
 namespace Hazel 
 {
-	std::string FileDialogs::OpenFile(const char* filter)
+	std::optional<std::string> FileDialogs::OpenFile(const char* filter)
 	{
 		OPENFILENAMEA ofn;
 		CHAR szFile[260] = { 0 };
@@ -27,10 +27,10 @@ namespace Hazel
 		{
 			return ofn.lpstrFile;
 		}
-		return std::string();
+		return std::nullopt;
 	}
 
-	std::string FileDialogs::SaveFile(const char* filter)
+	std::optional<std::string> FileDialogs::SaveFile(const char* filter)
 	{
 		OPENFILENAMEA ofn;
 		CHAR szFile[260] = { 0 };
@@ -41,6 +41,7 @@ namespace Hazel
 		ofn.nMaxFile = sizeof(szFile);
 		ofn.lpstrFilter = filter;
 		ofn.nFilterIndex = 1;
+		ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
 		// Sets the default extension by extracting it from the filter
 		ofn.lpstrDefExt = std::strchr(filter, '\0') + 1;
 		ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
@@ -48,6 +49,6 @@ namespace Hazel
 		{
 			return ofn.lpstrFile;
 		}
-		return std::string();
+		return std::nullopt;
 	}
 }
