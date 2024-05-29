379ae1297c216fc42b514714d1e3719d67a90992
Fallshadow <www.sunshuchao@outlook.com>
2024-05-29 16:55:02 +0800

    add 编辑器相机

--- Hazel/src/Hazel/Scene/Scene.h
+++ Hazel/src/Hazel/Scene/Scene.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "entt.hpp"
+#include "Hazel/Renderer/EditorCamera.h"
 
-#include "Hazel/Core/Timestep.h"
+#include "entt.hpp"
 
 namespace Hazel
 {
@@ -17,7 +17,8 @@ namespace Hazel
 		Entity CreateEntity(const std::string& name = std::string());
 		void DestroyEntity(Entity entity);
 
-		void OnUpdate(Timestep ts);
+		void OnUpdateRuntime(Timestep ts);
+		void OnUpdateEditor(Timestep ts, EditorCamera& camera);
 		void OnViewportResize(uint32_t width, uint32_t height);
 
 		Entity GetPrimaryCameraEntity();
