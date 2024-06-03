db826ada52b9797b6ce1cde2b3bebbb01a737fc2
Fallshadow <www.sunshuchao@outlook.com>
2024-06-03 14:39:51 +0800

    add 图形 显示文件

--- Hazelnut/src/Panels/ContentBrowserPanel.h
+++ Hazelnut/src/Panels/ContentBrowserPanel.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <filesystem>
+#include "Hazel/Renderer/Texture.h"
 
 namespace Hazel 
 {
@@ -13,5 +14,8 @@ namespace Hazel
 
 	private:
 		std::filesystem::path m_CurrentDirectory;
+
+		Ref<Texture2D> m_DirectoryIcon;
+		Ref<Texture2D> m_FileIcon;
 	};
 }
