3885271daee8c6d69c639e0e2a3be11e75700760
Fallshadow <www.sunshuchao@outlook.com>
2024-06-06 17:47:52 +0800

    fix 运行场景后新建场景 再次运行 显示的还是之前的
    因为运行时m_ActiveScene是复制了m_EditorScene，它们两个各指向不同的场景内存，当New空场景，m_ActiveScene又新指向了内存，原先指向的内存会销毁，但是m_EditorScene指向的场景内存还存在。
    
    先让m_EditorScene指向新场景内存，旧的编辑场景内存会被销毁，再让m_ActiveScene也指向新场景内存，它指向的旧的运行场景内存也会被销毁。

--- Hazelnut/src/EditorLayer.cpp
+++ Hazelnut/src/EditorLayer.cpp
@@ -521,7 +521,9 @@ namespace Hazel
 
 	void EditorLayer::NewScene()
 	{
-		m_ActiveScene = CreateRef<Scene>();
+		m_EditorScene = CreateRef<Scene>();
+		m_ActiveScene = m_EditorScene;
+
 		m_ActiveScene->OnViewportResize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
 		m_SceneHierarchyPanel.SetContext(m_ActiveScene);
 
