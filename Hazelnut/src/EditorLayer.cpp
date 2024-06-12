68857ce2b52249130bc6fb2bc193a0933d821034
Fallshadow <www.sunshuchao@outlook.com>
2024-06-12 19:01:15 +0800

    add & fix 复制、删除、整理
    - 修复了复制实体时的崩溃(实体名称字符串内存会被移动，之后再被引用)
    - 删除键现在从场景中删除实体
    - 移动Box2D体类型转换功能到新的Physics2D
    - 暴露的Rigidbody2D体类型获取/设置为c# API
    - 杂项

--- Hazelnut/src/EditorLayer.cpp
+++ Hazelnut/src/EditorLayer.cpp
@@ -229,10 +229,12 @@ namespace Hazel
 
 		ImGui::Begin("Stats");
 
+#if 0
 		std::string name = "None";
 		if (m_HoveredEntity)
 			name = m_HoveredEntity.GetComponent<TagComponent>().Tag;
 		ImGui::Text("Hovered Entity: %s", name.c_str());
+#endif
 
 		auto stats = Hazel::Renderer2D::GetStats();
 		ImGui::Text("Renderer2D Stats:");
@@ -504,6 +506,19 @@ namespace Hazel
 				}
 				break;
 			}
+			case Key::Delete:
+			{
+				if (Application::Get().GetImGuiLayer()->GetActiveWidgetID() == 0)
+				{
+					Entity selectedEntity = m_SceneHierarchyPanel.GetSelectedEntity();
+					if (selectedEntity)
+					{
+						m_SceneHierarchyPanel.SetSelectedEntity({});
+						m_ActiveScene->DestroyEntity(selectedEntity);
+					}
+				}
+				break;
+			}
 		}
 
 		return false;
@@ -591,6 +606,8 @@ namespace Hazel
 	{
 		if (Project::Load(path))
 		{
+			ScriptEngine::Init();
+
 			auto startScenePath = Project::GetAssetFileSystemPath(Project::GetActive()->GetConfig().StartScene);
 			OpenScene(startScenePath);
 			m_ContentBrowserPanel = CreateScope<ContentBrowserPanel>();
