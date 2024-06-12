0477ccf9a114c3cde27af38c47328ad2fd723c81
Fallshadow <www.sunshuchao@outlook.com>
2024-06-13 07:24:17 +0800

    add font纹理图集

--- Hazel/src/Hazel/Renderer/Font.h
+++ Hazel/src/Hazel/Renderer/Font.h
@@ -1,10 +1,23 @@
 #pragma once
 
+#include <filesystem>
+
+#include "Hazel/Core/Base.h"
+#include "Hazel/Renderer/Texture.h"
+
 namespace Hazel 
 {
+	struct MSDFData;
+
 	class Font
 	{
 	public:
 		Font(const std::filesystem::path& font);
+		~Font();
+
+		Ref<Texture2D> GetAtlasTexture() const { return m_AtlasTexture; }
+	private:
+		MSDFData* m_Data;
+		Ref<Texture2D> m_AtlasTexture;
 	};
 }
